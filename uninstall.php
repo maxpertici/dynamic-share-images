c3062ef2ab16ce8275109d6313080bcf4e6d8b48
Maxime Pertici <maxime@whodunit.fr>
2022-06-05 18:56:45 +0100

    unisntall comment

--- uninstall.php
+++ uninstall.php
@@ -5,12 +5,14 @@
  * 
  */
 
-// todo : remove uploads folder
+// todo : TEST TEST TEST
 
 $wp_upload_dir = wp_get_upload_dir() ;
 $dsi_uploads_dir = $wp_upload_dir['baseurl'] . '/dynamic-share-images/' ;
 
 dsimages_rrmdir( $dsi_uploads_dir ) ;
+
+// @source : https://kapeli.com/dash_share?docset_file=PHP&docset_name=PHP&path=www.php.net/manual/en/function.rmdir.html&platform=php&repo=Main&source=www.php.net/manual/en/function.rmdir.php
 
 function dsimages_rrmdir( $dir ){ 
     if (is_dir($dir)) { 
