cc8465e59b1692f3033d86f1ba6de66ca5e11a39
Maxime Pertici <maxime@whodunit.fr>
2022-06-06 07:58:11 +0200

    wip unistall via hook

--- dynamic-share-images.php
+++ dynamic-share-images.php
@@ -193,7 +193,21 @@ function dsimages_generate_share_image( $post ){
 function dsimages_uninstall(){
  
   echo 'unistall';
-  
   die ;
   
+  $wp_upload_dir   = wp_get_upload_dir() ;
+  $dsi_uploads_dir = $wp_upload_dir['baseurl'] . '/dynamic-share-images/' ;
+
+  // @source : https://kapeli.com/dash_share?docset_file=PHP&docset_name=PHP&path=www.php.net/manual/en/function.rmdir.html&platform=php&repo=Main&source=www.php.net/manual/en/function.rmdir.php
+  if (is_dir($dir)) { 
+    $objects = scandir($dir); 
+    foreach ($objects as $object) { 
+      if ($object != "." && $object != "..") { 
+        if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
+      } 
+    } 
+    reset($objects); 
+    rmdir($dir); 
+  } 
+ 
 }
